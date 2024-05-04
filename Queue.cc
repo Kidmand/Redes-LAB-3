5d8ad1428a5ed260e6bb4ca72a4cc65bd3e38f5b
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2024-05-04 19:00:25 -0300

    feat: Implementacion de la primera parte de la consigna.

--- Queue.cc
+++ Queue.cc
@@ -6,14 +6,19 @@
 
 using namespace omnetpp;
 
-class Queue: public cSimpleModule {
+class Queue : public cSimpleModule
+{
 private:
     cQueue buffer;
     cMessage *endServiceEvent;
     simtime_t serviceTime;
+    cOutVector bufferSizeVector;
+    cOutVector packetDropVector;
+
 public:
     Queue();
     virtual ~Queue();
+
 protected:
     virtual void initialize();
     virtual void finish();
@@ -22,43 +27,72 @@ protected:
 
 Define_Module(Queue);
 
-Queue::Queue() {
+Queue::Queue()
+{
     endServiceEvent = NULL;
 }
 
-Queue::~Queue() {
+Queue::~Queue()
+{
     cancelAndDelete(endServiceEvent);
 }
 
-void Queue::initialize() {
+void Queue::initialize()
+{
     buffer.setName("buffer");
     endServiceEvent = new cMessage("endService");
+
+    // Inicializar bufferSizeVector si es necesario
+    bufferSizeVector.setName("BufferSizeVector"); // Establecer el nombre del vector de salida
+    bufferSizeVector.record(0);                   // Inicializar el vector con un valor inicial (puede ser el tamaño inicial del buffer)
+
+    // Inicializar packetDropVector si es necesario
+    packetDropVector.setName("PacketDropVector");
+    packetDropVector.record(0); // Inicializar con 0, si no hay descartes al inicio de la simulación
 }
 
-void Queue::finish() {
+void Queue::finish()
+{
 }
 
-void Queue::handleMessage(cMessage *msg) {
+void Queue::handleMessage(cMessage *msg)
+{
 
     // if msg is signaling an endServiceEvent
-    if (msg == endServiceEvent) {
+    if (msg == endServiceEvent)
+    {
         // if packet in buffer, send next one
-        if (!buffer.isEmpty()) {
+        if (!buffer.isEmpty())
+        {
             // dequeue packet
-            cMessage *pkt = (cMessage*) buffer.pop();
+            cPacket *pkt = (cPacket *)buffer.pop();
             // send packet
             send(pkt, "out");
             // start new service
-            serviceTime = par("serviceTime");
+            serviceTime = pkt->getDuration();
             scheduleAt(simTime() + serviceTime, endServiceEvent);
         }
-    } else { // if msg is a data packet
-        // enqueue the packet
-        buffer.insert(msg);
-        // if the server is idle
-        if (!endServiceEvent->isScheduled()) {
-            // start the service
-            scheduleAt(simTime(), endServiceEvent);
+    }
+    else
+    {
+        // Verificar si el buffer está lleno
+        if (buffer.getLength() >= par("bufferSize").intValue())
+        {
+            delete msg; // Descartar el paquete
+            this->bubble("packet dropped");
+            packetDropVector.record(1);
+        }
+        else
+        {
+            // Enqueue the packet
+            buffer.insert(msg);
+            bufferSizeVector.record(buffer.getLength());
+            // If the server is idle
+            if (!endServiceEvent->isScheduled())
+            {
+                // Start the service
+                scheduleAt(simTime() + 0, endServiceEvent);
+            }
         }
     }
 }
